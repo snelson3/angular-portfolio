b3166d0a0ef03fab7bf52cbf5a3533e7b68a6288
Sam Nelson <snelson3@cs.uoregon.edu>
2015-12-23 16:32:09 -0800

    got repeater working

--- app/ui/app.js
+++ app/ui/app.js
@@ -1,1 +1,6 @@
-var portfolio = angular.module('portfolio', []);
+var portfolio = angular.module('portfolio', ['ngRoute'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/portfolio', {templateUrl: 'ui/portfolio.html', controller: 'dataController'});
+  $routeProvider.otherwise({redirectTo: 'portfolio'});
+}]);
--- app/ui/data.controller.js
+++ app/ui/data.controller.js
@@ -1,5 +1,6 @@
-portfolio.controller('dataController', ['$scope','$http', 'contactService',
+portfolio.controller('dataController', ['$scope','$http', 'dataService',
   function($scope,$http,dataService) {
+
     dataService.getProjects().success(function(data) {
       $scope.projects = data;
     });
