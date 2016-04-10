95ad6befc25edbc0276c4cb4a6411e2f781daddd
Sam Nelson <snelson3@cs.uoregon.edu>
2016-04-10 16:49:27 -0700

    added resume page, making stylistic changes

--- app/ui/app.js
+++ app/ui/app.js
@@ -4,5 +4,6 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/portfolio', {templateUrl: 'ui/portfolio.html', controller: 'dataController', url:'projects'});
   $routeProvider.when('/school', {templateUrl: 'ui/portfolio.html', controller: 'dataController', url:'school'});
   $routeProvider.when('/ml', {templateUrl: 'ui/portfolio.html', controller: 'dataController', url: 'ml'});
+  $routeProvider.when('/resume', {templateUrl: 'ui/resume.html', controller: 'dataController', url: 'resume'});
   $routeProvider.otherwise({redirectTo: 'portfolio'});
 }]);
